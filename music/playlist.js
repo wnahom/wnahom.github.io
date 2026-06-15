f4bfcb88e2c2c143c61d29355cb106e78dfdf126
mohan <wnahom@users.noreply.github.com>
2026-06-15 23:37:33 +0800

    Site updated: 2026-06-15 23:37:31

---
+++ music/playlist.js
@@ -0,0 +1,20 @@
+var LOCAL_SONGS = [
+  {
+    "name": "Andrew Prahlow - Travelers' encore",
+    "title": "Travelers' encore",
+    "artist": "Andrew Prahlow",
+    "src": "audio/Andrew Prahlow - Travelers' encore.mp3"
+  },
+  {
+    "name": "George Winston - Variations On The Canon By Pachelbel",
+    "title": "Variations On The Canon By Pachelbel",
+    "artist": "George Winston",
+    "src": "audio/George Winston - Variations On The Canon By Pachelbel.mp3"
+  },
+  {
+    "name": "山岡晃 - Promise",
+    "title": "Promise",
+    "artist": "山岡晃",
+    "src": "audio/山岡晃 - Promise.mp3"
+  }
+];
