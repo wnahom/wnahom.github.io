29df69ba0175190a315fbe840db6348361987fa0
mohan <wnahom@users.noreply.github.com>
2026-06-17 00:19:57 +0800

    Site updated: 2026-06-17 00:19:56

--- music/playlist.js
+++ music/playlist.js
@@ -12,6 +12,24 @@ var LOCAL_SONGS = [
     "src": "audio/George Winston - Variations On The Canon By Pachelbel.mp3"
   },
   {
+    "name": "Video Games Live,The City of Prague Philharmonic Orchestra - Silent Hill™ 2 - Theme of Laura”",
+    "title": "Silent Hill™ 2 - Theme of Laura”",
+    "artist": "Video Games Live,The City of Prague Philharmonic Orchestra",
+    "src": "audio/Video Games Live,The City of Prague Philharmonic Orchestra - Silent Hill™ 2 - Theme of Laura”.mp3"
+  },
+  {
+    "name": "乐进大将军 - 森林狂想曲 钢琴独奏",
+    "title": "森林狂想曲 钢琴独奏",
+    "artist": "乐进大将军",
+    "src": "audio/乐进大将军 - 森林狂想曲 钢琴独奏.mp3"
+  },
+  {
+    "name": "坂本龍一 - Merry Christmas Mr. Lawrence",
+    "title": "Merry Christmas Mr. Lawrence",
+    "artist": "坂本龍一",
+    "src": "audio/坂本龍一 - Merry Christmas Mr. Lawrence.mp3"
+  },
+  {
     "name": "山岡晃 - Promise",
     "title": "Promise",
     "artist": "山岡晃",
